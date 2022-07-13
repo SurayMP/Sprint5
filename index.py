78ad15f753739a81d2853df79c2f155ea1ee32f3
Juancito <cjuan.chona@hotmail.com>
2022-07-13 14:23:07 -0300

    Captura del archivo json en index y transformacion de la info en diccionario

--- index.py
+++ index.py
@@ -1,5 +1,13 @@
+import json
 if __name__=='__main__':
     # from clases.clientes import Cliente
     # Arrastrar y soltar archivo json
-    ArchivoArrastrado=input('Arrastre El Archivo Json con la Data')
-    print(ArchivoArrastrado)
+    ArchivoArrastrado=input('Arrastre El Archivo Json con la Informacion y Presione Enter \n : ')
+    try:
+        archivo = open(ArchivoArrastrado,'r')
+        # La Variable Data tiene la info del archivo
+        data=json.load(archivo)
+        archivo.close()
+    except Exception as error:
+        print("Un Error A Ocurrido con el Archivo Proporcionado")
+        # print(error)
