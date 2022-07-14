aca0d6ed9b02ded05fdf0543e4131d85a9b98dce
Ignacio Latorre <ignaciolatorreap@gmail.com>
2022-07-14 02:03:36 -0300

    clase cuenta cumple con el diagrama de clases
    pensar como inicializar correctamente

--- clases/cuenta.py
+++ clases/cuenta.py
@@ -1,6 +1,19 @@
 # Varias clases con los tipos de cuentas o le asignamos un tipo a una variable?
 class Cuenta:
-    def __init__(self,direccionCompleta) -> None:
+    
+    _limite_extraccion_diario: float
+    _limite_transferencia_recibida: float
+    _monto: float
+    _costo_transferencias: float
+    _saldo_descubierto_transferible: float
+
+    def __init__(self) -> None:
         """ DireccionCompleta Diccionario? o Array? """
-        print(direccionCompleta)
-        pass
+        self._limite_extraccion_diario = 0
+        self._limite_transferencia_recibida = 0
+        self._monto = 0
+        self._costo_transferencias = 0
+        self._saldo_descubierto_transferible = 0
+
+
+
